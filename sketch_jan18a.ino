9b5491ae93fe22e5f908ac9ab818aade20ff3de7
Whitewolf1026 <aaronb1026@hotmail.com>
2018-01-22 08:16:20 -0500

    Add files via upload
    jan18a- keypad display values in serial monitor and set key to be trigger for other action

---
+++ sketch_jan18a.ino
@@ -0,0 +1,31 @@
+#include <Keypad.h>
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //three columns
+char keys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'0', 'F', 'E', 'D'}
+};
+byte rowPins[ROWS] = {10, 11, 12, 13}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {6, 7, 8, 9}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  char key = keypad.getKey();
+  if (key) {
+    switch (key){
+      case '1':
+      Serial.println("You win");
+      break;
+      default:
+      Serial.println(key);
+    }
+  }
+}
