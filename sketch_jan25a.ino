c6d58c26817121773c579e7658c16b061b712e6b
Whitewolf1026 <aaronb1026@hotmail.com>
2018-01-25 13:26:52 -0500

    Add files via upload
    jan25a- Using a button and piezo speaker to record the duration of button presses and play them back as sound. Default code records five button presses but can be modified to record any amount
    jan25b- Analog temperature sensor record and display temperature in serial monitor. Displays temperature in both Celsius/Centigrade and in Fahrenheit

---
+++ sketch_jan25a.ino
@@ -0,0 +1,27 @@
+unsigned long duration[7];
+int count, next, last;
+
+void setup() {
+  pinmode(7, INPUT_PULLUP);
+}
+
+void loop() {
+  int y = digitalWrite(7);
+
+  if (y == LOW) {
+    next = millis();
+    duration[count++] = next - last;
+    last = next;
+    tone(8, 440, 100);
+    delay(200);
+  }
+  if (count == 5) {
+    delay(1000);
+    for (int x = 1; x <= 5; x++) {
+      tone(8, 440, 100);
+      delay(duration[x]);
+    }
+    count = 0;
+  }
+}
+
---
+++ sketch_jan25b.ino
@@ -0,0 +1,22 @@
+int sensePin = A0;
+int sensorInput;
+double temp;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  sensorInput = analogRead(A0);
+  temp = (double)sensorInput / 1024; //find percentage of input
+  temp = temp * 5; //multiply by 5 to get voltage
+  temp = temp * 100; //comment this line out to convert back to volts
+  Serial.print("Current Temperature In °C: ");
+  Serial.print(temp);
+  Serial.print(" °C   Current Temperature In °F: ");
+  temp = temp * 9/5;
+  temp = temp + 32;
+  Serial.print(temp);
+  Serial.println(" °F");
+  delay(200);
+}
