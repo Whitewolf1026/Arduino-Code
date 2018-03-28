ed032c1eada1a428cd1e8b77d781835cb52b5f59
Whitewolf1026 <aaronb1026@hotmail.com>
2018-03-28 08:26:11 -0400

    Add files via upload
    mar16a-line rotating inside a circle
    mar20a-rudimentary game of pong (base code not playable but should be able to be modified to do so in another sketch)
    mar27a-three-dimensional cube rotating at different angles in middle of screen

---
+++ sketch_mar16a.ino
@@ -0,0 +1,42 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_MOSI   9
+#define OLED_CLK   10
+#define OLED_DC    11
+#define OLED_CS    12
+#define OLED_RESET 13
+Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+int hSpin = 30;
+int vSpin = 32;
+int SpinRadius = 50;
+
+void setup() {
+  display.begin(SSD1306_SWITCHCAPVCC);
+
+}
+
+void loop() {
+  for (int i = 0; i < 60; i++) {                 //Change loop to "for(int i=0; i<60; i++){" to spin clockwise
+    display.fillRect(0, 0, 128, 64, BLACK);      //Blanks out last frame to black
+    display.setTextColor(WHITE);
+    display.setTextSize(3);
+    display.setCursor(74, 5);                                      // Position of text
+    display.print(i);
+    display.fillCircle(hSpin, vSpin, SpinRadius / 2, WHITE);        // Draws a white filled circle
+    display.fillCircle(hSpin, vSpin, (SpinRadius / 2) - 1 , BLACK); // draws a black circle to refresh line movement
+
+    float SpinAngle = i * 6;
+    SpinAngle = SpinAngle / 57.296;                                 // converts degrees to radians
+    int a = (hSpin + (sin(SpinAngle) * (SpinRadius / 2)));          //coordinate for end of line position horizontally
+    int b = (vSpin - (cos(SpinAngle) * (SpinRadius / 2)));          //coordinate for end of line position vertically
+
+    display.drawLine(hSpin, vSpin, a, b, WHITE); // draws rotating line from center out to horizontal and vertical position.
+    display.display();                       // refresh screen
+    delay(10);
+
+  }
+}
---
+++ sketch_mar20a.ino
@@ -0,0 +1,101 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_MOSI   9
+#define OLED_CLK   10
+#define OLED_DC    11
+#define OLED_CS    12
+#define OLED_RESET 13
+Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+void setup() {
+  display.begin(SSD1306_SWITCHCAPVCC);
+}
+
+void loop() {
+  pongExample();
+}
+
+void pongExample()
+{
+  int paddleW = 3;  // Paddle width
+  int paddleH = 15;  // Paddle height
+  // Paddle 0 (left) position coordinates
+  int paddle0_Y = (display.height() / 2) - (paddleH / 2);
+  int paddle0_X = 2;
+  // Paddle 1 (right) position coordinates
+  int paddle1_Y = (display.height() / 2) - (paddleH / 2);
+  int paddle1_X = display.width() - 3 - paddleW;
+  int ball_rad = 2;  // Ball radius
+  // Ball position coordinates
+  int ball_X = paddle0_X + paddleW + ball_rad;
+  int ball_Y = random(1 + ball_rad, display.height() - ball_rad);//paddle0_Y + ball_rad;
+  int ballVelocityX = 1;  // Ball left/right velocity
+  int ballVelocityY = 1;  // Ball up/down velocity
+  int paddle0Velocity = -1;  // Paddle 0 velocity
+  int paddle1Velocity = 1;  // Paddle 1 velocity
+
+  while ((ball_X - ball_rad > 1) &&
+         (ball_X + ball_rad < display.width() - 2))
+  {
+    ball_X += ballVelocityX;
+    ball_Y += ballVelocityY;
+    // Check if the ball hit the left paddle
+    if (ball_X - ball_rad < paddle0_X + paddleW)
+    {
+      // Check if ball is within paddle's area
+      if ((ball_Y > paddle0_Y) && (ball_Y < paddle0_Y + paddleH))
+      {
+        ball_X++;  // Move ball over one to the right
+        ballVelocityX = -ballVelocityX; // Change velocity
+      }
+    }
+    // Check if the ball hit the right paddle
+    if (ball_X + ball_rad > paddle1_X)
+    {
+      // Check if ball is within paddle's area
+      if ((ball_Y > paddle1_Y) && (ball_Y < paddle1_Y + paddleH))
+      {
+        ball_X--;  // Move ball over one to the left
+        ballVelocityX = -ballVelocityX; // change velocity
+      }
+    }
+    // Check if the ball hit the top or bottom
+    if ((ball_Y <= ball_rad) || (ball_Y >= (display.height() - ball_rad - 1)))
+    {
+      // Change up/down velocity direction
+      ballVelocityY = -ballVelocityY;
+    }
+    // Move the paddles up and down
+    paddle0_Y += paddle0Velocity;
+    paddle1_Y += paddle1Velocity;
+    // Change paddle 0's direction if it hit top/bottom
+    if ((paddle0_Y <= 1) || (paddle0_Y > display.height() - 2 - paddleH))
+    {
+      paddle0Velocity = -paddle0Velocity;
+    }
+    // Change paddle 1's direction if it hit top/bottom
+    if ((paddle1_Y <= 1) || (paddle1_Y > display.height() - 2 - paddleH))
+    {
+      paddle1Velocity = -paddle1Velocity;
+    }
+
+    // Draw the Pong Field
+    display.clearDisplay();  // Clear the page
+    // Draw an outline of the screen:
+    display.drawRect(0, 0, display.width() - 1, display.height(), WHITE);
+    // Draw the center line
+    display.fillRect(display.width() / 2 - 1, 0, 2, display.height(), WHITE);
+    // Draw the Paddles:
+    display.fillRect(paddle0_X, paddle0_Y, paddleW, paddleH, WHITE);
+    display.fillRect(paddle1_X, paddle1_Y, paddleW, paddleH, WHITE);
+    // Draw the ball:
+    display.fillCircle(ball_X, ball_Y, ball_rad, WHITE);
+    // Draw everything on the screen:
+    display.display();
+    delay(25);
+  }
+  delay(1000);
+}
